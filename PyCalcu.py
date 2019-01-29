2c2ea8a1ece1c2db094c584f2bc4712ffd7b6138
Flipp3rrr <33151653+Flipp3rrr@users.noreply.github.com>
2019-01-29 12:32:01 +0100

    Update PyCalcu.py
    Wrote the help functions and function 1 (add).

--- PyCalcu.py
+++ PyCalcu.py
@@ -1,9 +1,38 @@
-# PyCalcu by Flipp3rrr
+# ================ PyCalcu by Flipp3rrr ================
+
+# Basic information and help
 print("Starting PyCalcu...")
 print("Done!")
 print()
-print("For basic syntax help, type \"syntax\".")
-print("For a list of commands, type \"list commands\".")
-print("To start PyCalcu, type \"start\"")
+print("For basic syntax help, type \"Functions\".")
+print("For a list of commands, type \"Advanced functions\".")
+print("Type the wanted function")
+
+# Main loop
 while True:
-	input("> ")
+
+	# Main variable
+	input_var = input("> ")
+
+	# Function help
+	if input_var == "Functions":
+		print("The basic functions of this program are:")
+		print("1. Add")
+		print("2. Substract")
+		print("3. Multiply")
+		print("4. Divide")
+		print()
+		print("To use a function you can either use the number or name.")
+
+	# Advanced functions help
+	else if input_var == "Advanced functions":
+		print("For more advanced function you got the following possibilities:")
+		print("5. None")
+		print("6. None")
+		print("7. None")
+
+	# Add function
+	else if input_var == "Add" or input_var == "1":
+		num1 = int(input("First number > "))
+		num2 = int(input("Second number > "))
+		print(num1, "+", num2, "=", add(num1,num2))
