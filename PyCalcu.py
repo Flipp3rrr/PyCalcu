80ad7ebaf57b0bf5f3b77cddc23d87570fe74333
Flipp3rrr <33151653+Flipp3rrr@users.noreply.github.com>
2019-02-12 18:35:50 +0100

    I broke by broken stuff even more... Send help...

--- PyCalcu.py
+++ PyCalcu.py
@@ -1,12 +1,7 @@
 # ================ PyCalcu by Flipp3rrr ================
 
 # Basic information and help
-print("Starting PyCalcu...")
-print("Done!")
-print()
-print("For basic syntax help, type \"Functions\".")
-print("For a list of commands, type \"Advanced functions\".")
-print("Type the wanted function")
+print("Welcome to PyCalcu. Type \"Help\" or \"1\" to get started.")
 
 # Main loop
 while True:
@@ -15,27 +10,47 @@ while True:
 	input_var = input("> ")
 
 	# Function help
-	if input_var == "Functions":
+	if input_var == "Help" or input_var == "1":
 		print("The basic functions of this program are:")
-		print("1. Add")
-		print("2. Substract")
-		print("3. Multiply")
-		print("4. Divide")
-		print()
+		print("1. Help")
+		print("2. Advanced help")
+		print("3. Add")
+		print("4. Subtract")
+		print("5. Multiply")
+		print("6. Divide")
+		print("")
 		print("To use a function you can either use the number or name.")
 
 	# Advanced functions help
-	else if input_var == "Advanced functions":
+	elif input_var == "Advanced help" or input_var == "2":
 		print("For more advanced function you got the following possibilities:")
-		print("5. None")
-		print("6. None")
-		print("7. None")
+		print("7. Percents")
+		print("8. None")
+		print("9. None")
 
 	# Add function
-	else if input_var == "Add" or input_var == "1":
-		num1 = int(input("First number > "))
-		num2 = int(input("Second number > "))
-		print(num1, "+", num2, "=", add(num1,num2))
+	elif input_var == "Add" or input_var == "3":
+		num1 = int(input("Starting number > "))
+		num2 = int(input("How many do you want to add to ", num1, "? > "))
+		print(num1, "+", num2, "=", num1 + num2)
+
+	# Subtract function
+	elif input_var == "Subtract" or input_var == "4":
+		num1 = int(input("Starting number > "))
+		num2 = int(input("How many do you want to subtract from ",num1, "? > "))
+		print(num1, "-", num2, "=", num1 - num2)
+
+	# Multiply function
+	elif input_var == "Multiply" or input_var == "5":
+		num1 = int(input("Starting number > "))
+		num2 = int(input("By how many do you want to multiply ", num1, "? > "))
+		print(num1, "-", num2, "=", num1 * num2)
+
+	# Divide function
+	elif input_var == "Divide" or input_var == "6":
+		num1 = int(input("Starting number > "))
+		num2 = int(input("By how many do you want to divide ", num1, "? > "))
+		print(num1, "-", num2, "=", num1 / num2)
 
 	# Invalid function reporter
 	else:
